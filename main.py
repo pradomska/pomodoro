9d7e269f22cff1563ba7df05121ba052255e515c
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-16 14:10:50 +0200

    add files

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+
+canvas = Canvas(width=200, height=223)
+image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=image)
+
+
+
+window.mainloop()
---
+++ readme.md
@@ -0,0 +1,5 @@
+pomodoro-app
+---
+Using tkinter, I'm going to building a Pomodoro app. 
+It's about the Pomodoro technique, famous book by Francesco Cirillo.
+Technique that helps people time-manage and to get more stuff done. 
