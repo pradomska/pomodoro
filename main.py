3ae14d64a1d17d56ca18c9081ec2d1e1b6528302
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-16 14:33:33 +0200

    add buttons, labels and checkmark

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tkinter import *
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+YELLOW = "#fde7c3"
+WHITE = "#ffeaea"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -17,11 +18,24 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=223)
+label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
+label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=image)
+canvas.create_image(100, 110, image=image)
+canvas.create_text(100, 130, text="00:00", fill=WHITE, font=(FONT_NAME, 28, "bold"))
+canvas.grid(column=1, row=1)
 
+button_start = Button(text="Start", font=(FONT_NAME, 10, "normal"))
+button_start.grid(column=0, row=2)
 
+button_stop = Button(text="Stop", font=(FONT_NAME, 10, "normal"))
+button_stop.grid(column=2, row=2)
+
+check = Label(text="✓", fg=GREEN, font=(FONT_NAME, 20, "bold"), bg=YELLOW)
+check.grid(column=1, row=3)
 
 window.mainloop()
